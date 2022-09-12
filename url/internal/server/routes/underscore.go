2064f51f6ab081dff076e089acfbea3a2f80acf9
Paulo <git@pauloo27.dev>
2022-09-11 23:08:08 -0300

    ref: move version to URLService

--- url/internal/server/routes/underscore.go
+++ url/internal/server/routes/underscore.go
@@ -3,13 +3,16 @@ package routes
 import (
 	"net/http"
 
+	"github.com/Pauloo27/shurl/url/internal/server/middlewares"
+	"github.com/Pauloo27/shurl/url/internal/service"
 	"github.com/go-chi/chi/v5"
 )
 
 func RouteUnderscore() http.Handler {
 	r := chi.NewRouter()
 	r.Get("/version", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("SHURL v0.0.1 PRE-ALPHA"))
+		service := r.Context().Value(middlewares.ServiceCtxKey).(*service.URLService)
+		w.Write([]byte("SHURL " + service.Version))
 	})
 	return r
 }
--- url/internal/service/service.go
+++ url/internal/service/service.go
@@ -2,14 +2,18 @@ package service
 
 import "database/sql"
 
+const Version = "0.0.1"
+
 type URLService struct {
 	Config
-	DB *sql.DB
+	DB      *sql.DB
+	Version string
 }
 
 func NewService(config *Config, db *sql.DB) *URLService {
 	return &URLService{
-		Config: *config,
-		DB:     db,
+		Config:  *config,
+		DB:      db,
+		Version: Version,
 	}
 }
